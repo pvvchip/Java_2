d49707fd6ba8039288d72657e4c51f7dc506af67
pvvchip <pvvchip@mail.ru>
2018-08-30 17:36:28 +0300

    lesson 3

---
+++ src/com/home/java2/PhoneBook.java
@@ -0,0 +1,15 @@
+package com.home.java2;
+
+import java.util.HashMap;
+
+public class PhoneBook {
+    protected HashMap<String, String> pb;
+
+    public HashMap<String, String> getPb() {
+        return pb;
+    }
+
+    public void setPb(HashMap<String, String> pb) {
+        this.pb = pb;
+    }
+}
